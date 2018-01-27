d3a785b7402d22be359d24ff047bf50e4e7f9816
murat aka <murat-aka@hotmail.com>
2018-01-27 13:31:42 +0000

    exercise3

--- program.js
+++ program.js
@@ -1,15 +1,56 @@
- //console.log("HELLO WORLD");
+
+//////////////////////////////////////////
+/* this program contains exercises from */
+/* freecodecamps nodejs tutorials       */
+//////////////////////////////////////////
+
+
+         //////////////////
+         ///// START  /////
+         //////////////////
+
+
+ /*////////////////////////////////*/
+ /* function baby steps exercise 2 */
+ /*////////////////////////////////*/
  
- //console.log(process.argv);
+ function getSum(){
  
- var sum = 0;
+   var sum = 0;
+
+   for(var i=2;i<process.argv.length;i++){ // loop through arguments and add them up
+   
+       sum+= Number(process.argv[i]); // arguments start from index 2
+   }
+   
+   console.log(sum);
  
- 
- 
- for(var i=2;i<process.argv.length;i++){ //loop through arguments and add them up
-     
-     sum+= Number(process.argv[i]); //arguments start from index 2
-     
  }
  
- console.log(sum);
+ //getSum();
+ 
+ 
+/////////////////////////////////////////
+/*  function exercise 2  MY FIRST I/O  */
+/////////////////////////////////////////
+
+
+
+function syncIO(){
+ 
+   
+   
+   var fs = require('fs'); // load file system module
+   var fFile = process.argv[2]; // read file path from args
+   var bufferObject = fs.readFileSync(fFile); // read file contents to the buffer
+   var strString = bufferObject.toString(); // convert file contents or the buffer object to string
+   var arrArray = strString.split('\n'); // divide new lines to array elements
+   var count = arrArray.length - 1; // count number of lines, note the last line doesnt end with new line i.e '\n'
+   
+   console.log(count);
+ 
+ 
+}
+
+
+  syncIO();
