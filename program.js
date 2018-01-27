248ad59dcb9ec608df857d396ca635b1f8a0477b
murat aka <murat-aka@hotmail.com>
2018-01-27 14:10:06 +0000

    exercise4

--- program.js
+++ program.js
@@ -31,14 +31,10 @@
  
  
 /////////////////////////////////////////
-/*  function exercise 2  MY FIRST I/O  */
+/*  function exercise 3  MY FIRST I/O  */
 /////////////////////////////////////////
 
-
-
 function syncIO(){
- 
-   
    
    var fs = require('fs'); // load file system module
    var fFile = process.argv[2]; // read file path from args
@@ -48,9 +44,36 @@ function syncIO(){
    var count = arrArray.length - 1; // count number of lines, note the last line doesnt end with new line i.e '\n'
    
    console.log(count);
- 
- 
+
 }
 
 
-  syncIO();
+  //syncIO();
+  
+////////////////////////////////////////////////
+/*  function exercise 4  MY FIRST ASYNC I/O!  */
+////////////////////////////////////////////////
+
+function asyncIO(){
+   
+   var fs = require('fs'); // load file system module
+   var fFile = process.argv[2]; // read file path from args
+
+   fs.readFile(fFile, function doneReading(err, fileContents) { // readFile is asycronous so a call back function is passed.
+    
+    if(!err){
+     
+      var strString = fileContents.toString(); // convert file contents or the buffer object to string
+      var arrArray = strString.split('\n'); // divide new lines to array elements
+      var count = arrArray.length - 1; // count number of lines, note the last line doesnt end with new line i.e '\n' 
+      console.log(count);     
+     
+    }
+
+   });
+
+}
+
+
+  //asyncIO();
+
